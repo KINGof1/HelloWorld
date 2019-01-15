ec81f5fa33da81b92ddf3d50ee2fd28d8e5d12d3
Robert <Robert.scott190@gmail.com>
2019-01-15 11:49:37 -0500

    1st Message

---
+++ HelloWorld.test/ProgramTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace HelloWorld.test
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void Hello_World_Test()
+        {
+            Assert.Equal("Hello", "Hello");
+            //BIGG COMMENT
+        }
+    }
+}
